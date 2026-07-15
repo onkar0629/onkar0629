b690918098b3d44b96e16b89c501112eff19a27d
Onkar Ankush Jadhav <113853487+onkar0629@users.noreply.github.com>
2026-07-15 18:29:13 +0530

    Implement user profile query in main function
    Refactor main function to execute PROFILE_QUERY and display user information.

--- generator/main.py
+++ generator/main.py
@@ -1,9 +1,27 @@
+from config import GITHUB_USERNAME
 from client import GitHubClient
-from queries import VIEWER_QUERY
+from queries import PROFILE_QUERY
 
 
-client = GitHubClient()
+def main():
 
-result = client.execute(VIEWER_QUERY)
+    client = GitHubClient()
 
-print(result)
+    result = client.execute(
+        PROFILE_QUERY,
+        {
+            "login": GITHUB_USERNAME
+        }
+    )
+
+    user = result["data"]["user"]
+
+    print(f"Username     : {user['login']}")
+    print(f"Name         : {user['name']}")
+    print(f"Followers    : {user['followers']['totalCount']}")
+    print(f"Following    : {user['following']['totalCount']}")
+    print(f"Repositories : {user['repositories']['totalCount']}")
+
+
+if __name__ == "__main__":
+    main()
