2fc9feeeff0944dcde0ff2903808853decef66ff
Onkar Ankush Jadhav <113853487+onkar0629@users.noreply.github.com>
2026-07-15 18:28:52 +0530

    Enhance execute method with variables and error handling
    Updated the execute method to accept variables and handle errors.

--- generator/client.py
+++ generator/client.py
@@ -1,28 +1,32 @@
 import requests
 
-from config import (
-    GRAPHQL_URL,
-    GITHUB_TOKEN
-)
+from config import GRAPHQL_URL, GITHUB_TOKEN
 
 
 class GitHubClient:
 
     def __init__(self):
-
         self.headers = {
             "Authorization": f"Bearer {GITHUB_TOKEN}"
         }
 
-    def execute(self, query: str):
+    def execute(self, query: str, variables=None):
 
         response = requests.post(
             GRAPHQL_URL,
-            json={"query": query},
+            json={
+                "query": query,
+                "variables": variables or {}
+            },
             headers=self.headers,
             timeout=30
         )
 
         response.raise_for_status()
 
-        return response.json()
+        data = response.json()
+
+        if "errors" in data:
+            raise Exception(data["errors"])
+
+        return data
